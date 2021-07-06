254f81bec558a28b5e3d8d2727f2200eaafd5ef8
Stackbit Code Editor <projects@stackbit.com>
2021-07-06 22:35:03 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -40,7 +40,7 @@ export default class Body extends React.Component {
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
                     )}
                     {(font === 'fraunces') ? (
-                    <link rel="preload" as="style" href="">
+                    <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Fraunces:ital,wght@0,400;0,500;1,400;1,500&display=swap">
                     <link href="https://fonts.googleapis.com/css2?family=Fraunces:ital,wght@0,400;0,500;1,400;1,500&display=swap" rel="stylesheet"/>
                     ) : ((font === 'karla') && (
                     <link href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/>
