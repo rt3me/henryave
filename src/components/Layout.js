0005d6629c5090312dcdc55d40ffdeefff5e7097
Stackbit Code Editor <projects@stackbit.com>
2021-08-03 01:59:52 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -56,7 +56,7 @@ export default class Body extends React.Component {
                     {this.props.children}
                   </main>
                   <Footer {...this.props} />
-                  <
+                  <Tawto />
                 </div>
             </React.Fragment>
         );
