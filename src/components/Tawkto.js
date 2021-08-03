2351760494f6ae3c6ecdb40d07e41bd8dfa09bf2
Stackbit Code Editor <projects@stackbit.com>
2021-08-03 15:21:07 +0000

    Tawkto.js: changed

--- src/components/Tawkto.js
+++ src/components/Tawkto.js
@@ -4,9 +4,9 @@ import _ from 'lodash';
 import tawkTo from "tawkto-react";
 
 export default class Tawkto extends React.Component {
-    const tawkToPropertyId = '5bc2274208387933e5bb3791';
+    const tawkToPropertyId = '5bc2274208387933e5bb3791'
 
-    const tawkToKey = '3e4278d7d0206a2680985105a5ea00ff65212ba6';
+    const tawkToKey = '3e4278d7d0206a2680985105a5ea00ff65212ba6'
 
     componentDidMount(){
         tawkTo(tawkToPropertyId, tawkToKey)
