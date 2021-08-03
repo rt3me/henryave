60a93a727f497e6e2eb43cea3690b49dbabfdb35
Stackbit Code Editor <projects@stackbit.com>
2021-08-03 01:41:23 +0000

    Tawkto.js: changed

--- src/components/Tawkto.js
+++ src/components/Tawkto.js
@@ -4,14 +4,14 @@ import _ from 'lodash';
 import tawkTo from "tawkto-react";
 
 export default class Tawkto extends React.Component {
-const tawkToPropertyId = '5bc2274208387933e5bb3791'
+    const tawkToPropertyId = '5bc2274208387933e5bb3791'
 
-// Direct Chat Link
-// https://tawk.to/chat/tawkToPropertyId/tawkToKey
+    // Direct Chat Link
+    // https://tawk.to/chat/tawkToPropertyId/tawkToKey
 
-const tawkToKey = '3e4278d7d0206a2680985105a5ea00ff65212ba6'
+    const tawkToKey = '3e4278d7d0206a2680985105a5ea00ff65212ba6'
 
-componentDidMount(){
-    tawkTo(tawkToPropertyId, tawkToKey)
-}
+    componentDidMount(){
+        tawkTo(tawkToPropertyId, tawkToKey)
+    }
 }
