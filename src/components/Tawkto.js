f6dc96c2a421ac394efcb42b7b5ba1094605cd04
Stackbit Code Editor <projects@stackbit.com>
2021-08-03 15:21:52 +0000

    Tawkto.js: changed

--- src/components/Tawkto.js
+++ src/components/Tawkto.js
@@ -8,7 +8,7 @@ import tawkTo from "tawkto-react";
 
     const tawkToKey = '3e4278d7d0206a2680985105a5ea00ff65212ba6'
 
-    componentDidMount() {
+    componentDidMount(){
         tawkTo(tawkToPropertyId, tawkToKey)
     }
 //}
