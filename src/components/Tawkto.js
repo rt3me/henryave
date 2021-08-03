69d08f36b9394daf041b1ca4e15aa4f30eb90524
Stackbit Code Editor <projects@stackbit.com>
2021-08-03 02:00:46 +0000

    Tawkto.js: changed

--- src/components/Tawkto.js
+++ src/components/Tawkto.js
@@ -6,9 +6,6 @@ import tawkTo from "tawkto-react";
 export default class Tawkto extends React.Component {
     const tawkToPropertyId = '5bc2274208387933e5bb3791'
 
-    // Direct Chat Link
-    // https://tawk.to/chat/tawkToPropertyId/tawkToKey
-
     const tawkToKey = '3e4278d7d0206a2680985105a5ea00ff65212ba6'
 
     componentDidMount(){
